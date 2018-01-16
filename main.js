18f003c8756274dadce2a2b7e721cd2ea210a99e
santoshm <dosantoshm@gmail.com>
2018-01-15 22:13:08 -0500

    crud en vue.js

---
+++ main.js
@@ -0,0 +1,64 @@
+function findById(items, id){
+	for(var i in items){
+		if (items[i].id == id) {
+			return items[i];
+		}
+	}
+	return null;
+}
+
+
+var vm = new Vue({
+	el: 'body',
+	data: {
+		notes: [
+			{
+				note: 'Laravel lts',
+				category_id: 1
+			},
+			{
+				note: 'diretiva v-for en vue',
+				category_id: 2
+			},
+			{
+				note: 'seo organico',
+				category_id: 3
+			},
+			{
+				note: 'evento on click en vue',
+				category_id: 2
+			}
+		],
+		categories: [
+			{
+				id: 1,
+				name: 'Laravle'
+			},
+			{
+				id: 2,
+				name: 'vue.js'
+			},
+			{
+				id:3,
+				name:'Prublicidad'
+			}
+		]
+	},
+	methods: {
+		deleteNote: function(note){
+			this.notes.$remove(note);
+		},
+		editNote: function(note){
+			Vue.set(note, 'editing', true);
+		},
+		updateNote: function(note){
+			note.editing = false;
+		}
+	},
+	filters: {
+		category: function (id){
+			var category = findById(this.categories, id);
+			return category != null ? category.name : 'Sin categoria';
+		}
+	}
+});
